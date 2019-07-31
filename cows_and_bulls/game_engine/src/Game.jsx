7d22dc3744a53e8a479960fb733d21a66eebce9b
Shalini Pyapali <shalinipyapali@fb.com>
2019-07-30 23:14:04 -0700

    Styled the game using material-ui

--- cows_and_bulls/game_engine/src/Game.jsx
+++ cows_and_bulls/game_engine/src/Game.jsx
@@ -1,67 +1,109 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import WordBar from "./WordBar";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 const WORD = "LAKE";
 
-class Game extends Component {
-  state = {
-    gameEnded: false,
-    guess: "",
-    originalWord: WORD,
-    cows: 0,
-    bulls: 0
+
+function MadeWithLoveByShalini() {
+  return (
+    <Typography variant="body2" color="textSecondary">
+      {'Made with love by Shalini'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    padding: theme.spacing(2),
+    marginTop: 'auto',
+    backgroundColor: 'white',
+  },
+  }));
+  
+
+
+function Game() {
+
+  const [guess, setGuess] = useState("");
+  const classes = useStyles();
+
+  function handleGuess(newGuess) {
+    setGuess(newGuess);
   };
 
-  handleGuess = newGuess => {
-    this.setState({ guess: newGuess });
-  };
-
-  addCharsToSet = () => {
+  function addCharsToSet() {
     let chars = new Set();
-    for (let char of this.state.originalWord) {
+    for (let char of WORD) {
       chars.add(char);
     }
     return chars;
   };
 
-  revealCowsandBulls = () => {
-    const chars = this.addCharsToSet();
+  
+  function revealCowsandBulls() {
+    const chars = addCharsToSet();
     let charsSeenSoFar = new Set();
     let bulls = 0,
       cows = 0;
-    for (let index = 0; index < this.state.guess.length; index++) {
-      let currentChar = this.state.guess[index];
-      if (currentChar === this.state.originalWord[index]) {
+    for (let index = 0; index < guess.length; index++) {
+      let currentChar = guess[index];
+      if (currentChar === WORD[index]) {
         bulls++;
       } else if (!charsSeenSoFar.has(currentChar) && chars.has(currentChar)) {
         cows++;
       }
+      charsSeenSoFar.add(currentChar);
     }
-    return this.checkIfGameHasEnded(cows, bulls);
+    return checkIfGameHasEnded(cows, bulls);
   };
 
-  checkIfGameHasEnded = (cows, bulls) => {
+  function checkIfGameHasEnded(cows, bulls) {
     if (bulls === 4) {
-      return <h3>Congrats! You've won the game</h3>;
+      return <Typography variant="h5" component="h2" color="primary" gutterButtom>Congrats! You've won the game!</Typography>;
     } else {
       return (
         <div>
-          <h1>Cows: {cows}</h1>
-          <h1>Bulls: {bulls}</h1>
+          <Typography variant="h5" component="h2" gutterButtom>Cows: {cows}</Typography>
+          <Typography variant="h5" component="h2" gutterButtom>Bulls: {bulls}</Typography>
         </div>
       );
     }
   };
 
-  render() {
-    return (
-      <div>
-        <h1>Please enter a four character word below</h1>
-        <WordBar onGuess={this.handleGuess} />
-        <h1>{this.revealCowsandBulls()}</h1>
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Container component="main" className={classes.main} maxWidth="sm">
+        <Typography variant="h2" component="h1" gutterBottom>
+          Welcome to Cows and Bulls!
+        </Typography>
+        <Typography variant="h5" component="h2" gutterBottom>
+          {'Please enter a word below.\n'}
+          <WordBar onGuess={handleGuess} />
+          {revealCowsandBulls()}
+        </Typography>
+      </Container>
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="body1">Thank you for playing.</Typography>
+          <MadeWithLoveByShalini/>
+        </Container>
+      </footer>
       </div>
-    );
-  }
+  );
 }
 
 export default Game;
